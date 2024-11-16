69a8205461af0fdb72bf4651aa05c3bb02f3b1be
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-11-16 16:12:58 +0300

    fix build

--- src/pages/home/homePartnersDetailPage.tsx
+++ src/pages/home/homePartnersDetailPage.tsx
@@ -7,32 +7,38 @@ import { Button, Checkbox, Switch, Tabs, TabsProps } from "antd";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
+interface CustomTabItem {
+  key: string;
+  label: React.ReactNode;
+  text: string;
+}
+
 const HomePartnersDetailPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { setSelectPartner } = useActions();
   const { selectPartner } = useAppSelector(partnersSelector);
 
-  const tabData: TabsProps["items"] = [
+  const tabData: CustomTabItem[] = [
     {
       key: "1",
-      label: "Автомобиль",
-      children: <CarContact />,
+      label: "Конструктор продуктов",
+      text: ERoutesNames.HOME_CONSTRUCTOR,
     },
     {
       key: "2",
-      label: "Документы",
-      children: "Информация об документе",
+      label: "Реестр договоров",
+      text: ERoutesNames.HOME_REESTR,
+    },
+    {
+      key: "3",
+      label: "Реестр партнеров",
+      text: ERoutesNames.HOME_PARTNERS,
     },
     {
       key: "4",
-      label: "Стоимость",
-      children: "Информация о стоимость",
-    },
-    {
-      key: "5",
-      label: "Оплата",
-      children: "Информация об оплата",
+      label: "Реестр клиентов",
+      text: ERoutesNames.HOME_CLIENTS,
     },
   ];
 
